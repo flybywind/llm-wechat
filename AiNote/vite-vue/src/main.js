91dceed842669cbebfaee6f948e7d7cd19a3a701
tongxin.wen <tongxinwen@fintopia.tech>
2024-11-30 23:11:16 +0800

    electron build support

--- AiNote/vite-vue/src/main.js
+++ AiNote/vite-vue/src/main.js
@@ -7,6 +7,24 @@ import App from "./App.vue";
 
 library.add(faPencilAlt);
 
+document.addEventListener(
+  "deviceready",
+  function () {
+    console.log("Cordova is ready!");
+    // 您的 Cordova 相关代码
+    window.resolveLocalFileSystemURL(
+      cordova.file.dataDirectory,
+      function (dirEntry) {
+        console.log("file system open: " + dirEntry.name);
+        // var isAppend = true;
+        // createFile(dirEntry, "fileToAppend.txt", isAppend);
+      }
+      // onErrorLoadFs
+    );
+  },
+  false
+);
+
 const app = createApp(App);
 app.component("font-awesome-icon", FontAwesomeIcon);
 app.mount("#app");
